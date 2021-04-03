85ffdd2d06af1f3ad82fd28f832f2b8fc278b02f
Mattia (Win10) <mattia.pesenti@polimi.it>
2021-04-03 14:26:20 +0200

    Init project Master_Read for students

--- 02-I2C_Master_Read.cydsn/main.c
+++ 02-I2C_Master_Read.cydsn/main.c
@@ -10,12 +10,71 @@
  * ========================================
 */
 #include "project.h"
+#include "../src_shared/I2C_Interface.h"
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    I2C_Peripheral_Start();
+    UART_1_Start();
+    
+    CyDelay(5); //"The boot procedure is complete about 5 ms after device power-up."
+    
+    // String to print out messages on the UART
+    char message[50] = {'\0'};
+    
+    UART_1_PutString("**************\r\n");
+    UART_1_PutString("** I2C Scan **\r\n");
+    UART_1_PutString("**************\r\n");
+    
+    CyDelay(10);
+    
+    uint32_t rval;
+ 
+	// Setup the screen and print the header
+	UART_1_PutString("\n\n   ");
+	for(uint8_t i = 0; i<0x10; i++)
+	{
+        sprintf(message, "%02X ", i);
+		UART_1_PutString(message);
+	}
+ 
+	// SCAN the I2C BUS for slaves
+	for( uint8_t i2caddress = 0; i2caddress < 0x80; i2caddress++ ) {
+        
+		if(i2caddress % 0x10 == 0 ) {
+            sprintf(message, "\n%02X ", i2caddress);
+		    UART_1_PutString(message);
+        }
+ 
+		rval = I2C_Master_MasterSendStart(i2caddress, I2C_Master_WRITE_XFER_MODE);
+        
+        if( rval == I2C_Master_MSTR_NO_ERROR ) // If you get ACK then print the address
+		{
+            sprintf(message, "%02X ", i2caddress);
+		    UART_1_PutString(message);
+		}
+		else //  Otherwise print a --
+		{
+		    UART_1_PutString("-- ");
+		}
+        I2C_Master_MasterSendStop();
+	}
+	UART_1_PutString("\n\n");
+    
+    
+    /******************************************/
+    /*            I2C Reading                 */
+    /******************************************/
+    
+    /*      I2C Master Read - WHOAMI Register       */
+    
+    
+    /*      I2C Master Read - STATUS Register       */
+    
+    
 
     for(;;)
     {
