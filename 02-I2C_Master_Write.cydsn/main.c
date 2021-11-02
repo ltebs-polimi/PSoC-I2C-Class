25eb0c0d47dfce80baa6fe810707fe53eb76d008
Mattia (Win10) <mattia.pesenti@polimi.it>
2021-11-02 23:02:19 +0100

    Repo re-init

---
+++ 02-I2C_Master_Write.cydsn/main.c
@@ -0,0 +1,59 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <stdio.h>
+#include "project.h"
+#include "../src_shared/I2C_Interface.h"
+#include "../src_shared/LIS3DH.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    I2C_Peripheral_Start();
+    UART_1_Start();
+    
+    CyDelay(5); //"The boot procedure is complete about 5 ms after device power-up."
+    
+    // Check if LIS3DH is connected
+    uint32_t rval = I2C_Master_MasterSendStart(LIS3DH_DEVICE_ADDRESS, I2C_Master_WRITE_XFER_MODE);
+    if( rval == I2C_Master_MSTR_NO_ERROR ) {
+        UART_1_PutString("LIS3DH found @ address 0x18\r\n");
+    }
+    I2C_Master_MasterSendStop();
+    
+    // String to print out messages over UART
+    char message[50] = {'\0'};
+    
+    /******************************************/
+    /*            I2C Writing                 */
+    /******************************************/
+
+    
+    uint8_t ctrl_reg1;
+    ErrorCode error;
+    
+    // Your time to code!
+    // Write CTRL_REG1 and ...
+    
+    
+    // ... read the value of CTRL_REG1 and make sure
+    // you get the value written before
+    
+    
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
