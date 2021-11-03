9514116b8c8f09bdee4dd016511e12c27c7405fc
Mattia (Win10) <mattia.pesenti@polimi.it>
2021-11-03 18:11:16 +0100

    Changed macro value (0x07) to 0x47

--- 02-I2C_Master_Write.cydsn/main.c
+++ 02-I2C_Master_Write.cydsn/main.c
@@ -38,19 +38,19 @@ int main(void)
     /*            I2C Writing                 */
     /******************************************/
     
-    uint8_t ctrl_reg1;
+    uint8_t ctrl_reg1 = 0x00;
     ErrorCode error;
     
     // Your time to code!
     
     // Write CTRL_REG1 and...
-    if ( ctrl_reg1 != LIS3DH_NORMAL_MODE_OFF_CTRL_REG1 ) {
+    if ( ctrl_reg1 != 0x47 ) {
         UART_1_PutString("\r\nWriting to CTRL_REG1...\r\n");
         
         ctrl_reg1 = LIS3DH_NORMAL_MODE_CTRL_REG1;
         error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
                                              LIS3DH_CTRL_REG1,
-                                             LIS3DH_NORMAL_MODE_OFF_CTRL_REG1);
+                                             0x47);
     
         if( error == NO_ERROR ) {
             sprintf(message, "CTRL_REG1 successfully written as: 0x%02X\r\n", ctrl_reg1);
